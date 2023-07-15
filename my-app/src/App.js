72cda07e1c954d3a563129b90d7a7a8a63ee5ead
zaurkhalilli <ggencetel274@gmail.com>
2023-07-15 21:12:49 +0400

    Layout added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import Home from "./Pages/Home/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./Pages/Login/Login"
+import Pricing from "./Pages/Pricing/Pricing"
+import Layout from "./Layout/Layout";
 const App = () => {
   return (
     <div>
-      
+       <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route index path='/' element={<Home/>} />
+          <Route index path='/about' element={<Login/>} />
+          <Route index path='/blog' element={<Pricing/>} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
     </div>
   )
 }
---
+++ my-app/src/Layout/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from "../Navbar/Navbar.scss"
+import { Link } from 'react-router-dom';
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <div className="container">
+        <div className="row">
+            <div className="col-xl-2">
+                asad    
+            </div>
+            <div className="col-xl-6"></div>
+            <div className="col-xl-4"></div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
