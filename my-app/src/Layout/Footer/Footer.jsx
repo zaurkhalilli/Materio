d7f10f592010fd2a3e4279536f7b296ddc025f4a
zaurkhalilli <ggencetel274@gmail.com>
2023-07-22 16:58:33 +0400

    new footer adds

--- my-app/src/Layout/Footer/Footer.jsx
+++ my-app/src/Layout/Footer/Footer.jsx
@@ -5,7 +5,12 @@ const Footer = () => {
     <div className='footer'>
         <div className="container">
           <div className="row">
-            <div className="col-xl-4"></div>
+            <div className="col-xl-4">
+              <h4>MATERIO</h4>
+              <p>Most Powerful & Comprehensive 🤩 React NextJS Admin Template with Elegant Material Design & Unique Layouts.</p>
+              <input type="text" placeholder='Subscribe to newsletter' />
+              <button>SUBSCRIBE</button>
+            </div>
             <div className="col-xl-4"></div>
             <div className="col-xl-4"></div>
           </div>
