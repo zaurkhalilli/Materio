7c83137fad86143b834d3d981fdeffb3da451a84
zaurkhalilli <ggencetel274@gmail.com>
2023-07-23 22:56:20 +0400

    Footer new adds

--- my-app/src/Layout/Footer/Footer.jsx
+++ my-app/src/Layout/Footer/Footer.jsx
@@ -9,9 +9,26 @@ const Footer = () => {
               <h4>MATERIO</h4>
               <p>Most Powerful & Comprehensive 🤩 React NextJS Admin Template with Elegant Material Design & Unique Layouts.</p>
               <input type="text" placeholder='Subscribe to newsletter' />
-              <button>SUBSCRIBE</button>
+1              <button>SUBSCRIBE</button>
             </div>
-            <div className="col-xl-4"></div>
+            <div className="col-xl-4">
+              <div className="liststyle">
+                <ul> Demos
+                  <li><a href="">Vertical Layout</a></li>
+                  <li><a href="">Horizontal Layout</a></li>
+                  <li><a href="">Bordered Layout</a></li>
+                  <li><a href="">Semi Dark Layout</a></li>
+                  <li><a href="">Dark Layout</a></li>
+                </ul>
+                <ul>Pages
+                  <li>Pricing</li>
+                  <li>Payment</li>
+                  <li>Checkout</li>
+                  <li>Help Center</li>
+                  <li>Login/Register</li>
+                </ul>
+              </div>
+            </div>
             <div className="col-xl-4"></div>
           </div>
         </div>
