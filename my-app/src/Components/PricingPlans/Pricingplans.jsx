23a52e1ade3ca562ddd643a02955fd51b761bdfa
zaurkhalilli <ggencetel274@gmail.com>
2023-07-27 21:40:03 +0400

    Pricing page new adds

---
+++ my-app/src/Components/PricingPlans/Pricingplans.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pricingplans = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Pricingplans
--- my-app/src/Pages/Pricing/Pricing.js
+++ my-app/src/Pages/Pricing/Pricing.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import App from '../../App'
+import Layout from '../../Layout/Layout'
 const Pricing = () => {
   return (
     <div>
-      
+      <Layout>
+        
+      </Layout>
     </div>
   )
 }
