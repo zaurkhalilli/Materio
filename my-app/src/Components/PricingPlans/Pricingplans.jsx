c9619c41b04d2c0436c569e2a0cf4c8806434423
zaurkhalilli <ggencetel274@gmail.com>
2023-07-28 17:55:11 +0400

    Pricing Plans new adds

--- my-app/src/Components/PricingPlans/Pricingplans.jsx
+++ my-app/src/Components/PricingPlans/Pricingplans.jsx
@@ -2,8 +2,33 @@ import React from 'react'
 import styled from "../PricingPlans/Pricingplans.scss"
 const Pricingplans = () => {
   return (
-    <div className='pricingplans'>
-      
+    <div className='pricing'>
+      <div className="pricingplans">
+        <h2>Pricing Plans</h2>
+        <h6>All plans include 40+ advanced tools and features to boost your product.
+        Choose the best plan to fit your needs.</h6>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-4">
+            <img src="https://demos.themeselection.com/materio-bootstrap-html-admin-template/assets/img/illustrations/pricing-basic.png" alt="" />
+            <h5>Basic</h5>
+            <p>A simple start for everyone</p>
+            <sup>$ </sup> <span>0</span> <label>/month</label>
+            <li>100 responses a month</li>
+            <li>Unlimited forms and surveys</li>
+            <li>Unlimited fields</li>
+            <li>Basic form creation tools</li>
+            <li>Up to 2 subdomains</li>
+          </div>
+          <div className="col-xl-4">
+            <img src="https://demos.themeselection.com/materio-bootstrap-html-admin-template/assets/img/illustrations/pricing-standard.png" alt="" />
+          </div>
+          <div className="col-xl-4">
+            <img src="https://demos.themeselection.com/materio-bootstrap-html-admin-template/assets/img/illustrations/pricing-enterprise.png" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- my-app/src/Pages/Pricing/Pricing.js
+++ my-app/src/Pages/Pricing/Pricing.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import App from '../../App'
 import Layout from '../../Layout/Layout'
+import Pricingplans from '../../Components/PricingPlans/Pricingplans'
 const Pricing = () => {
   return (
     <div>
       <Layout>
-        
+        <Pricingplans/>
       </Layout>
     </div>
   )
