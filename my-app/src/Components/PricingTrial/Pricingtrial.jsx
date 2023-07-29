ef29a6f78011e58f64dccad6978cf2c245fa339f
zaurkhalilli <ggencetel274@gmail.com>
2023-07-29 20:38:06 +0400

    Pricing Trial buttons added

--- my-app/src/Components/PricingTrial/Pricingtrial.jsx
+++ my-app/src/Components/PricingTrial/Pricingtrial.jsx
@@ -3,7 +3,18 @@ import styled from "../PricingTrial/Pricingtrial.scss"
 const Pricingtrial = () => {
   return (
     <div className='trial'>
-      
+      <div className="container">
+        <div className="row">
+            <div className="col-xl-8">
+                <h3>Still not convinced? Start with a 14-day FREE trial!</h3>
+                <p>You will get full access to with all the features for 14 days.</p>
+                <button>START 14-DAY FREE TRIAL</button>
+            </div>
+            <div className="col-xl-4">
+            <img src="https://demos.themeselection.com/materio-bootstrap-html-admin-template/assets/img/illustrations/sitting-girl-with-laptop.png" alt="" />
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
