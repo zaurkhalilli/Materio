29c9bc36db6b8218baafc591caf43205735654b1
zaurkhalilli <ggencetel274@gmail.com>
2023-07-29 20:27:49 +0400

    PricingTrial added

---
+++ my-app/src/Components/PricingTrial/Pricingtrial.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from "../PricingTrial/Pricingtrial.scss"
+const Pricingtrial = () => {
+  return (
+    <div className='trial'>
+      
+    </div>
+  )
+}
+
+export default Pricingtrial
--- my-app/src/Pages/Pricing/Pricing.js
+++ my-app/src/Pages/Pricing/Pricing.js
@@ -2,11 +2,13 @@ import React from 'react'
 import App from '../../App'
 import Layout from '../../Layout/Layout'
 import Pricingplans from '../../Components/PricingPlans/Pricingplans'
+import Pricingtrial from '../../Components/PricingTrial/Pricingtrial'
 const Pricing = () => {
   return (
     <div>
       <Layout>
         <Pricingplans/>
+        <Pricingtrial/>
       </Layout>
     </div>
   )
