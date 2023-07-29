773407b935a1378e3cb46238525bc18d57c8bdaf
zaurkhalilli <ggencetel274@gmail.com>
2023-07-29 20:24:07 +0400

    Pricing Plans completed

--- my-app/src/Components/PricingPlans/Pricingplans.jsx
+++ my-app/src/Components/PricingPlans/Pricingplans.jsx
@@ -30,14 +30,14 @@ const Pricingplans = () => {
             <div className="img2">
             <img src="https://demos.themeselection.com/materio-bootstrap-html-admin-template/assets/img/illustrations/pricing-standard.png" alt="" />
             </div>
-            <h5>Basic</h5>
-            <p>A simple start for everyone</p>
+            <h5>Standard</h5>
+            <p>For small to medium businesses</p>
             <sup>$ </sup> <span>9</span> <label>/month</label>
-            <li>100 responses a month</li>
+            <li>Unlimited responses</li>
             <li>Unlimited forms and surveys</li>
-            <li>Unlimited fields</li>
-            <li>Basic form creation tools</li>
-            <li>Up to 2 subdomains</li>
+            <li>Instagram profile page</li>
+            <li>Google Docs integration</li>
+            <li>Custom “Thank you” page</li>
             <div className="middlebtn">
               <button>UPGRADE</button>
             </div>
@@ -46,14 +46,14 @@ const Pricingplans = () => {
             <div className="img3">
             <img src="https://demos.themeselection.com/materio-bootstrap-html-admin-template/assets/img/illustrations/pricing-enterprise.png" alt="" />
             </div>
-            <h5>Basic</h5>
-            <p>A simple start for everyone</p>
-            <sup>$ </sup> <span>0</span> <label>/month</label>
-            <li>100 responses a month</li>
-            <li>Unlimited forms and surveys</li>
-            <li>Unlimited fields</li>
-            <li>Basic form creation tools</li>
-            <li>Up to 2 subdomains</li>
+            <h5>Enterprise</h5>
+            <p>Solution for big organizations</p>
+            <sup>$ </sup> <span>19</span> <label>/month</label>
+            <li>PayPal payments</li>
+            <li>Logic Jumps</li>
+            <li>File upload with 5GB storage</li>
+            <li>Custom domain support</li>
+            <li>Stripe integration</li>
             <div className="rightbtn">
               <button>UPGRADE</button>
             </div>
