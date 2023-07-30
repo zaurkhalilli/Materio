4db84de7f071028043e587c31faac5b5f1c8dace
zaurkhalilli <ggencetel274@gmail.com>
2023-07-30 16:21:17 +0400

    Pricing Table added

---
+++ my-app/src/Components/PricingTable/Pricingtable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from "../PricingTable/Pricingtable.scss"
+const Pricingtable = () => {
+  return (
+    <div className='table'> 
+      
+    </div>
+  )
+}
+
+export default Pricingtable
