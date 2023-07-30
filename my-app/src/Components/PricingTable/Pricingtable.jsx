bc4e5c1bc59f9d2272941ff2cd16973110847595
zaurkhalilli <ggencetel274@gmail.com>
2023-07-30 17:03:05 +0400

    table added

--- my-app/src/Components/PricingTable/Pricingtable.jsx
+++ my-app/src/Components/PricingTable/Pricingtable.jsx
@@ -13,55 +13,55 @@ const Pricingtable = () => {
   <tr>
     <th>Features <span>Native Front Features</span> </th>
     <th>Starter <span>FREE</span></th>
-    <th>Country</th>
-    <th>Country</th>
+    <th>PRO <span>$7.5/Month</span></th>
+    <th>ENTERPRISE <span>$16/Month</span></th>
   </tr>
   <tr>
     <td>14-days free trial</td>
     <td><AiFillCheckCircle size={20} color='purple'/></td>
-    <td><AiFillCloseCircle color='gray' size={20}/></td>
+    <td><AiFillCheckCircle size={20} color='purple'/></td>
     <td><AiFillCheckCircle size={20} color='purple'/></td>
   </tr>
   <tr>
     <td>No user limit</td>
     <td><AiFillCloseCircle color='gray' size={20}/></td>
-    <td>Mexico</td>
+    <td><AiFillCloseCircle color='gray' size={20}/></td>
     <td><AiFillCheckCircle size={20} color='purple'/></td>
   </tr>
   <tr>
     <td>Product Support</td>
     <td><AiFillCloseCircle color='gray' size={20}/></td>
-    <td>Austria</td>
+    <td><AiFillCheckCircle size={20} color='purple'/></td>
     <td><AiFillCheckCircle size={20} color='purple'/></td>
   </tr>
   <tr>
     <td>Email Support</td>
     <td><AiFillCloseCircle color='gray' size={20}/></td>
-    <td>UK</td>
+    <td><AiFillCheckCircle size={20} color='purple'/></td>
     <td><AiFillCheckCircle size={20} color='purple'/></td>
   </tr>
   <tr>
     <td>Integrations</td>
     <td><AiFillCloseCircle color='gray' size={20}/></td>
-    <td>Canada</td>
+    <td><AiFillCheckCircle size={20} color='purple'/></td>
     <td><AiFillCheckCircle size={20} color='purple'/></td>
   </tr>
   <tr>
     <td>Removal of Front branding	</td>
     <td><AiFillCloseCircle color='gray' size={20}/></td>
-    <td>Italy</td>
+    <td><AiFillCheckCircle size={20} color='purple'/></td>
     <td><AiFillCheckCircle size={20} color='purple'/></td>
   </tr>
   <tr>
     <td>Active maintenance & support</td>
     <td><AiFillCloseCircle color='gray' size={20}/></td>
-    <td>Italy</td>
+    <td><AiFillCloseCircle color='gray' size={20}/></td>
     <td><AiFillCheckCircle size={20} color='purple'/></td>
   </tr>
   <tr>
     <td>Data storage for 365 days</td>
     <td><AiFillCloseCircle color='gray' size={20}/></td>
-    <td>Italy</td>
+    <td><AiFillCloseCircle color='gray' size={20}/></td>
     <td><AiFillCheckCircle size={20} color='purple'/></td>
   </tr>
   
