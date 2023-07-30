2246d5473be040f56ca995ad29aee1e0d6b862b0
zaurkhalilli <ggencetel274@gmail.com>
2023-07-30 16:49:09 +0400

    new adds

--- my-app/src/Components/PricingTable/Pricingtable.jsx
+++ my-app/src/Components/PricingTable/Pricingtable.jsx
@@ -1,9 +1,63 @@
 import React from 'react'
 import styled from "../PricingTable/Pricingtable.scss"
+import {AiFillCheckCircle} from "react-icons/ai"
+import {AiFillCloseCircle} from "react-icons/ai"
 const Pricingtable = () => {
   return (
     <div className='table'> 
-      
+    <div className="tabletxt">
+    <h3>Pick a plan that works best for you</h3>
+    <p>Stay cool, we have a 48-hour money back guarantee!</p>
+    </div>
+      <table>
+  <tr>
+    <th>Features <span>Native Front Features</span> </th>
+    <th>Starter <span>FREE</span></th>
+    <th>Country</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>14-days free trial</td>
+    <td><AiFillCheckCircle size={20} color='purple'/></td>
+    <td><AiFillCloseCircle color='gray' size={20}/></td>
+  </tr>
+  <tr>
+    <td>No user limit</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Product Support</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Email Support</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Integrations</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Removal of Front branding	</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+  <tr>
+    <td>Active maintenance & support</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+  <tr>
+    <td>Data storage for 365 days</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+  
+</table>
     </div>
   )
 }
--- my-app/src/Pages/Pricing/Pricing.js
+++ my-app/src/Pages/Pricing/Pricing.js
@@ -3,12 +3,14 @@ import App from '../../App'
 import Layout from '../../Layout/Layout'
 import Pricingplans from '../../Components/PricingPlans/Pricingplans'
 import Pricingtrial from '../../Components/PricingTrial/Pricingtrial'
+import Pricingtable from '../../Components/PricingTable/Pricingtable'
 const Pricing = () => {
   return (
     <div>
       <Layout>
         <Pricingplans/>
         <Pricingtrial/>
+        <Pricingtable/>
       </Layout>
     </div>
   )
